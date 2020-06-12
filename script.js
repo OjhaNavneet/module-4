839a79125975568315b5b2a7908217cbc6df71bc
OjhaNavneet <61623685+OjhaNavneet@users.noreply.github.com>
2020-06-12 23:05:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+
+
+for(var i=0 ; i<a.length; i++)
+{
+    var c=a[i].charAt(0);
+    if(c =="j" || c =="J")
+    console.log("Good Bye " + a[i]);
+    else
+    console.log("Hello " + a[i]);
+}
